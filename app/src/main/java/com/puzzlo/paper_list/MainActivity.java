e5bae18b6310ee4ed9f2e4e725f0da03ca2080dc
Puzzlo <p_uzo@mail.ru>
2015-02-05 13:41:03 +0300

    Initial commit

---
+++ app/src/main/java/com/puzzlo/paper_list/MainActivity.java
@@ -0,0 +1,59 @@
+package com.puzzlo.paper_list;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    String[] paper, glue;
+    ArrayAdapter<String> lvAdapterPaper, lvAdapterGlue;
+    ListView lvPaper, lvGlue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // make adapter for paper
+        paper = getResources().getStringArray(R.array.listOfPaper);
+        lvAdapterPaper = new ArrayAdapter<String>(this, R.layout.item_of_paper, paper);
+        lvPaper = (ListView) findViewById(R.id.listPaper);
+        lvPaper.setAdapter(lvAdapterPaper);
+        // make adapter for glue
+        glue = getResources().getStringArray(R.array.listOfGlue);
+        lvAdapterGlue = new ArrayAdapter<String>(this, R.layout.item_of_paper, glue);
+        lvGlue = (ListView) findViewById(R.id.listGlue);
+        lvGlue.setAdapter(lvAdapterGlue);
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
